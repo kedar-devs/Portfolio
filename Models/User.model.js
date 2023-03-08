1babf7b02ea994cdab3cf65b82ba9d37a8be5bc1
Kedar Devasthali <67545340+kedar-devs@users.noreply.github.com>
2023-03-08 12:33:21 +0530

    Feat:Add About and description in User Model

--- Models/User.model.js
+++ Models/User.model.js
@@ -3,6 +3,8 @@ const Schema=mongoose.Schema
 const UserSchema=Schema({
     name:{type:String,required:true},
     email:{type:String,required:true,unique:true},
+    about:{type:String,required:true},
+    description:{type:String,required:true},
     password:{type:String,required:true},
     accessToken:{type:String,required:true},
     resetToken:{type:String,required:true}
