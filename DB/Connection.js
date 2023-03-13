78f3e33e9eb95b6fd895b79148d67b3983b63efc
Kedar Devasthali <67545340+kedar-devs@users.noreply.github.com>
2023-03-13 11:50:24 +0530

    Feat:Add Nodemon and establish connection

--- DB/Connection.js
+++ DB/Connection.js
@@ -1,22 +1,20 @@
 const mongoose = require("mongoose");
-require('dotenv').config()
-const uri=process.env.MONGODB_URL
-exports.ConnectDB=()=>{
-	try {
-mongoose.connect(uri, {
-	useNewUrlParser: true,
-	useCreateIndex: true,
-	useUnifiedTopology: true,
-	useFindAndModify: false,
-	dbName:'classMate'
-});
+require("dotenv").config();
+const uri = process.env.MONGODB_URL;
+exports.ConnectDB = () => {
+  try {
+    mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName: "Portfolio",
+    });
 
-const connection=mongoose.connection
-return connection.once('open',()=>{
-    console.log('connection established successfully')
-})
-}catch(err){
-	console.log(err)
-    return false
-}
-}
+    const connection = mongoose.connection;
+    return connection.once("open", () => {
+      console.log("connection established successfully");
+    });
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+};
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app=require('./app')
 const port=process.env.PORT||5000
 const {ConnectDB}=require('./DB/Connection')
+const connect= ConnectDB()
 if(connect!=false){
     app.listen(port, () => {
         console.log(`Server is up on port ${port} ...`);
