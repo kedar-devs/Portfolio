06baa92685b5f1721ed96c001871bff1c8756eee
Kedar Devasthali <67545340+kedar-devs@users.noreply.github.com>
2023-04-05 13:09:42 +0530

    Feat:Dyanmicaly load the skills

--- client/src/Component/Card/SkillsCard.js
+++ client/src/Component/Card/SkillsCard.js
@@ -1,22 +1,37 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import { ScrollMenu, VisibilityContext,Arrow } from "react-horizontal-scrolling-menu";
 import "react-horizontal-scrolling-menu/dist/styles.css";
 import nodejs from "./../../assets/NodeJs.png";
-import Mongo from "./../../assets/Mongo.png";
+import {api} from './../Api/axiosRequest'
 
 function SkillsCard() {
+  const [skills,setSkills]=useState([])
+  const [loading,setLoading]=useState(true)
+  useEffect(()=>{
+    api.get('/Skill/GetAllSkills/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJqZWN0Ijoia2VkYXJkMjQ5LmtkQGdtYWlsLmNvbSIsImlhdCI6MTY3ODc3NTI2MX0.RIc9W1h6psOH3kQ9GMlTuVDUCh6bN1BURsg11nAHmVk')
+    .then(result=>{
+      setSkills(result.data)
+      setLoading(false)
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+  },[])
+
 
   return (
     <div>
+      {!loading?
       <ScrollMenu
       >
-        <div className="w-2/3">
-          <img src={nodejs} />
-        </div>
-        <div className="w-2/3">
-          <img src={Mongo} />
-        </div>
-      </ScrollMenu>
+        {skills.map((ele=>{
+          return(
+            <div className="w-32 h-32 m-5">
+            <img src={ele.SkillLink} />
+          </div>)
+        }))}
+        
+      </ScrollMenu>:<></>}
     </div>
   );
 }
