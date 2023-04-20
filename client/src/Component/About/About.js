c3c28e0c9e55f522f53d02e2aceb8678450532c9
Kedar Devasthali <67545340+kedar-devs@users.noreply.github.com>
2023-04-20 11:26:22 +0530

    Fix:Add pading in achievment

--- client/src/Component/About/About.js
+++ client/src/Component/About/About.js
@@ -11,7 +11,7 @@ function About() {
       <div className='lg:m-4 lg:mt-20 mt-10'>
         <Skill />
       </div>
-        <div className='lg:mt-20 lg:m-4 mt-10'>
+        <div className='lg:mt-20 lg:p-4 mt-10'>
         <Achievment />
         </div>
     </div>
