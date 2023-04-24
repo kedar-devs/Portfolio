8d649e43d6752f3a4c7a5d56d635310cc0e50fb3
Kedar Devasthali <67545340+kedar-devs@users.noreply.github.com>
2023-04-24 17:52:49 +0530

    Fix:Update remove unnecessary loader

--- client/src/Component/Achievment/Achievment.js
+++ client/src/Component/Achievment/Achievment.js
@@ -29,14 +29,7 @@ function Achievment() {
     
     </div>
     </div>:<div className="flex h-screen w-screen justify-center items-center">
-              <Dna
-                visible={true}
-                height="180"
-                width="180"
-                ariaLabel="dna-loading"
-                wrapperStyle={{}}
-                wrapperClass="dna-wrapper"
-              />
+              
             </div>}
     </>
   )
--- client/src/Component/Card/SkillsCard.js
+++ client/src/Component/Card/SkillsCard.js
@@ -69,16 +69,7 @@ function SkillsCard() {
         }))}
         </Slider>
       :<>
-      <div className="flex h-screen w-screen justify-center items-center">
-              <Dna
-                visible={true}
-                height="180"
-                width="180"
-                ariaLabel="dna-loading"
-                wrapperStyle={{}}
-                wrapperClass="dna-wrapper"
-              />
-            </div>
+      
       </>}
     </div>
   );
